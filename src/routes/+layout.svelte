aca89688b2f19ddd75155731b1c8e6e1328e6b46
j.dsouza <j.dsouza@freetimecompany.nl>
2023-01-17 13:40:14 +0530

    preliminary implemenation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import '../app.css';
+</script>
+
+<h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<slot />
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
