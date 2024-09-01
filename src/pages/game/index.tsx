1578d7d4ca6f52ec852f528112efc018119a694e
Jonathan Dsouza <mail2jona@yhoo.com>
2024-09-01 21:04:36 +0200

    added title to game page

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -1,15 +1,46 @@
+import { PRODUCTION_URL } from "@/libs/config";
 import { PokemonGame } from "@/libs/packages/pokemon-game";
+import { Head } from "next/document";
 
 export default function Page() {
 	return (
-		<div className="container mt-10 t:mt-20">
-			<PokemonGame.Provider>
-				<PokemonGame.Loading>
-					<PokemonGame.Start />
-					<PokemonGame.Play />
-					<PokemonGame.End />
-				</PokemonGame.Loading>
-			</PokemonGame.Provider>
-		</div>
+		<>
+			<Head>
+				<title>Pokédex: Guess The Type</title>
+
+				<meta
+					name="description"
+					content="Explore the complete list of all Pokémon, including detailed information about each species, evolutions, types, and abilities. Your ultimate online Pokédex."
+				/>
+
+				<meta
+					name="keywords"
+					content="Pokédex, Pokémon list, Pokémon database, Pokémon types, Pokémon evolutions, Pokémon abilities, Pokémon species"
+				/>
+
+				<meta
+					property="og:title"
+					content="Complete Pokémon List - Your Ultimate Pokédex"
+				/>
+
+				<meta
+					property="og:description"
+					content="Discover every Pokémon in existence with our comprehensive online Pokédex. Find detailed stats, types, evolutions, and more."
+				/>
+
+				<meta property="og:url" content={PRODUCTION_URL + "/game"} />
+
+				<link rel="canonical" href={PRODUCTION_URL + "/game"} />
+			</Head>
+			<div className="container mt-10 t:mt-20">
+				<PokemonGame.Provider>
+					<PokemonGame.Loading>
+						<PokemonGame.Start />
+						<PokemonGame.Play />
+						<PokemonGame.End />
+					</PokemonGame.Loading>
+				</PokemonGame.Provider>
+			</div>
+		</>
 	);
 }
